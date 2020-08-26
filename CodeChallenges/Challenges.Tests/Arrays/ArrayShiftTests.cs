27fdd1e404e5530bef7090655e766f503f432a72
bkeyes86 <byronvkeyes@gmail.com>
2020-08-25 23:01:29 -0500

    chall2

---
+++ CodeChallenges/Challenges.Tests/Arrays/ArrayShiftTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeChallenges.Arrays;
+using Xunit;
+
+namespace Challenges.Tests.arrays
+{
+   public class ArrayShiftTests
+    {
+        [Fact]
+        public static void Shift_Array_add_num_in_middle_of_array()
+        {
+            int[] arrayToShift = new[] { 1, 2, 3, 4 };
+            int numToInsert = 6;
+
+            int[] result = ArrayShiftTests.insertShiftArray(arrayToShift, numToInsert);
+
+            Assert.Equal(new int[] { 1, 2, 6, 3, 4 }, result);
+        }
+    }
+}
