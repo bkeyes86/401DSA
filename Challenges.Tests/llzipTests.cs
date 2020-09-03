4d6fc19ab08cc08f0d1b40d7551f91b9c0cae4fc
bkeyes86 <byronvkeyes@gmail.com>
2020-09-02 22:34:38 -0500

    start going to add

---
+++ Challenges.Tests/llzipTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DataStructures;
+using Challenges;
+
+namespace Challenges.Tests
+{
+    class llzipTests
+    {
+    }
+}
---
+++ LLZip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructures;
+
+namespace challenges
+{
+
+
+	public class LLZip
+	{
+		public static zipLists(LinkedList listA, LinkedList listB)
+        {
+			Node currentA = listA.Head;
+			Node currentB = listB.Head;
+
+			while(currentA != null || currentB != null)
+            {
+
+            }
+        }
+	}
+}
